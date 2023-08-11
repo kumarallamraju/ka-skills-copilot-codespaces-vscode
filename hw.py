85d29fbaa4ba544384c4c6d81909e7abc28f0f78
kumarallamraju-msft <kumar.allamraju@gmail.com>
2023-08-11 23:38:36 +0000

    java commit

--- hw.py
+++ hw.py
@@ -4,5 +4,24 @@ print("hello world")
 def add(a, b):
     return a + b
 
-result = add(1, 2)
-print(result)
+def EvenorOdd(a):
+    if a % 2 == 0:
+        return "Even"
+    else:
+        return "Odd"
+    
+def isPrime(a):
+    if a == 1:
+        return False
+    else:
+        for i in range(2, a):
+            if a % i == 0:
+                return False
+        return True
+
+
+if __name__ == "__main__":
+    print("hello world")
+    print(add(1, 2))
+    print(isPrime(10))
+
